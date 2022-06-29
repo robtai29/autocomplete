5ff2b52539e808fbc2114cda6f6ac79d545eefb2
Jacques Jean-Gilles <jeangjac@amazon.com>
2022-06-29 11:55:51 -0400

    test class working on branch.

--- src/main/java/setup/FileManager.java
+++ src/main/java/setup/FileManager.java
@@ -24,18 +24,16 @@ public class FileManager {
     public static void setFile(String filePath) {
         file=new File(filePath);
     }
-    public static void scanFile(String filePath,TrieAutocomplete trieAutoInstance) throws IllegalArgumentException, SecurityException,FileNotFoundException {
+    public static void scanFile(TrieAutocomplete trieAutoInstance) throws FileNotFoundException {
         // read in the data
         Scanner in;
         HashMap<String, String> casingMap = new HashMap<String, String> ();
 
-        try {
-            in = new Scanner(new File(filePath), CHARSET);
+            in = new Scanner(file, CHARSET);
 
             int N = Integer.parseInt(in.nextLine());
             String[] terms = new String[N];
             double[] weights = new double[N];
-
             for (int i = 0; i < N; i++) {
                 String line = in.nextLine();
                 int tab = line.indexOf('\t');
@@ -44,29 +42,7 @@ public class FileManager {
                 terms[i] = line.substring(tab + 1).toLowerCase();
                 // create the autocomplete object
             }
-            System.out.println(47);
-//            System.out.println(Arrays.toString(terms));
-//            System.out.println(Arrays.toString(weights));
-             for (int i=0; i < N; i++) {
-                 System.out.println("term: " + terms[i] + "\t weights: " + weights[i] + "\t i: " + i);
-             }
-            System.out.println(Arrays.toString(weights));
             trieAutoInstance.setNodes(terms, weights);
-            System.out.println(49);
             trieAutoInstance.setCasingMap(casingMap);
-            System.out.println(50);
-
-            } catch (IllegalArgumentException
-                     | SecurityException e1) {
-                e1.printStackTrace();
-                System.exit(1);
-            } catch (FileNotFoundException e2) {
-                System.out.println("Cannot read file " + filePath);
-                System.exit(1);
-
-            }
-
-
-
     }
 }
